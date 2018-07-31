3acffc352e152f7c579cc9f83f22ce81562f7fc9
Joe Behrens <Joe.Behrens@rbaconsulting.com>
2018-07-31 14:02:04 -0500

    Modular refactor, removed SQL views creation

--- log-analysis.py
+++ log-analysis.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 
 import psycopg2
@@ -6,111 +6,76 @@ import time
 import datetime
 
 
-def create_top_articles_view():
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
+def execute_query(query, dbname):
+    """
+    Connects to a database defined by DBNAME,
+    runs a query defined by QUERY against the
+    database and returns the results of the query.
+    """
     try:
-        c.execute("""\
-        create view top_articles as
-            select articles.title, articles.author, count(log.id)
-            from log, articles
-            where '/article/' || articles.slug = log.path
-            group by articles.title, articles.author
-            order by count(log.id) desc;""")
-        db.commit()
-    except psycopg2.ProgrammingError:
-        c.execute("ROLLBACK;")
-        db.commit()
-    db.close()
+        db = psycopg2.connect("dbname={}".format(dbname))
+        c = db.cursor()
+        c.execute(query)
+        result = c.fetchall()
+        db.close()
+        return result
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
 
 
-def create_error_percentages_view():
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    try:
-        c.execute("""\
-            create view error_percentages as
-                select to_char(error_codes.day,'MM-DD-YYYY') as date,
-                trunc((error_codes.count) / (all_codes.count) * 100, 2)
-                as errors
-                from
-                (
-                    select date_trunc('day', log.time) as day,
-                    cast(count(*) as decimal(7,2)) from log
-                    where status != '200 OK' group by day
-                ) error_codes
-                join
-                (
-                select date_trunc('day', log.time) as day,
-                cast(count(*) as decimal(7,2)) from log group by day
-                ) all_codes on error_codes.day = all_codes.day;""")
-        db.commit()
-    except psycopg2.ProgrammingError:
-        c.execute("ROLLBACK;")
-        db.commit()
-    db.close()
+def print_top_articles(num):
+    """
+    Get the top number of articles by view
+    with limiter defined by NUM from the NEWS
+    database and print out the list along with
+    the number of views for that article by title.
+    """
+    top_articles = execute_query("select * from top_articles limit {};".format(num),"news")
+    print(' ')
+    print('TOP THREE ARTICLES')
+    print('==================')
+    for article in top_articles:
+        print('"{}" - {:,} views'.format(article[0], article[2]))
+    print(' ')
 
 
-def get_top_articles():
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute("select * from top_articles limit 3;")
-    top_articles = c.fetchall()
-    db.close()
-    return top_articles
-
-
-def get_top_authors():
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute("""\
+def print_top_authors():
+    """
+    Print out a list of authors ordered
+    by most views to least views.
+    """
+    top_authors = execute_query("""\
         select authors.name, sum(top_articles.count) as views
             from top_articles, authors
             where top_articles.author = authors.id
             group by authors.name
-            order by views desc;""")
-    top_authors = c.fetchall()
-    db.close()
-    return top_authors
+            order by views desc;""","news")
+    print(' ')
+    print('MOST POPULAR AUTHORS')
+    print('====================')
+    for author in top_authors:
+        print('{} - {:,} views'.format(author[0], author[1]))
+    print(' ')
 
 
-def get_top_errors_days():
-    db = psycopg2.connect("dbname=news")
-    c = db.cursor()
-    c.execute("""\
-        select date, errors
-            from error_percentages
-            where errors > 1;""")
-    top_errors_day = c.fetchall()
-    db.close()
-    return top_errors_day
+def print_top_error_days(percent):
+    """
+    Print a list of days where the percent
+    errors is greater than the provided percenatage
+    defined by PERCENT.
+    """
+    top_errors_days = execute_query(
+        "select date, errors from error_percentages where errors > {};".format(percent)
+        ,"news")
+    print(' ')
+    print('DAYS WITH MORE THAN 1% OF REQUESTS AS ERRORS')
+    print('============================================')
+    for error_day in top_errors_days:
+        print('{} - {}% errors'.format(error_day[0], error_day[1]))
+    print(' ')
 
 
-create_top_articles_view()
-create_error_percentages_view()
-top_articles = get_top_articles()
-top_authors = get_top_authors()
-top_errors_days = get_top_errors_days()
-
-print(' ')
-print('TOP THREE ARTICLES')
-print('==================')
-for article in top_articles:
-    print('"{}" - {:,} views'.format(article[0], article[2]))
-print(' ')
-
-print(' ')
-print('MOST POPULAR AUTHORS')
-print('====================')
-for author in top_authors:
-    print('{} - {:,} views'.format(author[0], author[1]))
-print(' ')
-
-print(' ')
-print('DAYS WITH MORE THAN 1% OF REQUESTS AS ERRORS')
-print('============================================')
-for error_day in top_errors_days:
-    converted_date = datetime.datetime.strptime(error_day[0], '%m-%d-%Y')
-    print('{} - {}% errors'.format(
-        time.strftime("%B %d, %Y", converted_date.timetuple()), error_day[1]))
-print(' ')
+if __name__ == '__main__':
+    print_top_articles(3)
+    print_top_authors()
+    print_top_error_days(1)
